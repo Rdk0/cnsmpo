72d8ab5e86bea9f3781258917ce80539e5ea8dcb
Rdk0 <radeklaufer@gmail.com>
2024-07-07 12:02:49 -0400

    second draft with plotly

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def MPO_pka(pka: float) -> float:
         return 1.0
     if pka >= 10.0:
         return 0.0
-    return (10- pka)/2.0  
+    return (10 - pka)/2.0  
 
 def MPO_hbd(hbd:int ) -> float:
     if hbd == 0:
@@ -50,27 +50,36 @@ def MPO_tpsa(tpsa: float) -> float:
     return 1.0 
 
 #add_selectbox = st.sidebar.selectbox(
-pka = st.sidebar.slider('pKa',min_value=0,max_value=14, value = 2),  # 👈 this is a widget
-mw = st.sidebar.slider('MolMass',min_value=0,max_value=1500, value = 300) 
-hbd = st.sidebar.slider('hbd',min_value=0,max_value=20, value = 2)
-clogp = st.sidebar.slider('clogP',min_value=0,max_value=7, value = 2)
-clogd = st.sidebar.slider('clogD',min_value=0,max_value=7, value = 2)
-tpsa = st.sidebar.slider('TPSA',min_value=0,max_value=300, value = 50)
+pka = st.sidebar.number_input('pKa',min_value=0,max_value=14, value = 2),  # 👈 this is a widget
+mw = st.sidebar.number_input('MolMass',min_value=0,max_value=1500, value = 300, step=10) 
+hbd = st.sidebar.number_input('hbd',min_value=0,max_value=20, value = 2)
+clogp = st.sidebar.number_input('clogP',min_value=0,max_value=7, value = 2)
+clogd = st.sidebar.number_input('clogD',min_value=0,max_value=7, value = 2)
+tpsa = st.sidebar.number_input("TPSA", 0, 300, value=50, step=10) #st.sidebar.slider('TPSA',min_value=0,max_value=300, value = 50)
 #) # 👈 this is a widget
 
-st.write('CNS MPO contribution for pKa is', MPO_pka(pka[0]))
-st.write('CNS MPO contribution for MolMass is', MPO_mw(mw))
-st.write('CNS MPO contribution for hbd', MPO_hbd(hbd))
-st.write('CNS MPO contribution for clogP', MPO_clogp(clogp))
-st.write('CNS MPO contribution for clogD', MPO_clogd(clogd))
-st.write('CNS MPO contribution for TPSA', MPO_tpsa(tpsa))
+#st.write('CNS MPO contribution for pKa is', MPO_pka(pka[0]))
+#st.write('CNS MPO contribution for MolMass is', MPO_mw(mw))
+#st.write('CNS MPO contribution for hbd', MPO_hbd(hbd))
+#st.write('CNS MPO contribution for clogP', MPO_clogp(clogp))
+#st.write('CNS MPO contribution for clogD', MPO_clogd(clogd))
+#st.write('CNS MPO contribution for TPSA', MPO_tpsa(tpsa))
+
 pka = MPO_pka(pka[0]) 
 mw = MPO_mw(mw) 
 hbd = MPO_hbd(hbd) 
 clogp = MPO_clogp(clogp)
 clogd = MPO_clogd(clogd)
 tpsa = MPO_tpsa(tpsa)
-data = {'pka' : [pka], 'mw': [mw], 'hbd': [hbd], 'clogp': [clogp], 'clogd': [clogd], 'tpsa': [tpsa]}
+data = {'pKa' : [pka], 'MW': [mw], 'HBD': [hbd], 'clogP': [clogp], 'clogD': [clogd], 'TPSA': [tpsa]}
 chart_data = pd.DataFrame(data)
 #st.bar_chart(source, x="variety", y="yield", color="site", horizontal=True)
-st.bar_chart(chart_data.transpose(), horizontal=False)
+pd.options.plotting.backend = "plotly"
+fig = chart_data.transpose().plot.bar()
+fig.update_layout(yaxis_title=None)
+fig.update_layout(xaxis_title=None) 
+fig.update_layout(showlegend=False)
+
+st.plotly_chart(fig)#, horizontal=False)
+
+st.text("CNS MPO =" + str(chart_data.sum(axis=1)[0]))
